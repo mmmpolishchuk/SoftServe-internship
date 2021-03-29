cbd587dd4a74c90e8f2427faa5920a0db068c20b
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-03-29 21:19:49 +0300

    add Slava

---
+++ ToDo/ToDo.Main/Migrations/20210329181851_AddedSlava.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDo.Migrations
+{
+    public partial class AddedSlava : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slava",
+                table: "ToDoItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slava",
+                table: "ToDoItems");
+        }
+    }
+}
