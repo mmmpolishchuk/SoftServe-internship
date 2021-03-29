69a3ec20880148bbe6ffa2a5484879ce1b6424aa
VyacheslavDzhus <slavadzhus158@gmail.com>
2021-03-29 21:28:51 +0300

    add migration Dzhus

---
+++ ToDo/ToDo.Main/Migrations/20210329182749_Dzhus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDo.Migrations
+{
+    public partial class Dzhus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dzhus",
+                table: "ToDoItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dzhus",
+                table: "ToDoItems");
+        }
+    }
+}
